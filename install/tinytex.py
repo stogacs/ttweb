860db4a343d0bfc57db5c78954239452240bd134
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 05:54:32 +0000

    Adding tarfile member sanitization to extractall()

--- install/tinytex.py
+++ install/tinytex.py
@@ -80,7 +80,26 @@ class TinyTexLoader(Loader):
             print('Unpacking release archive...')
             if ext is Ext.TARGZ or ext is Ext.TGZ:
                 with tarfile.open(archive_path, 'r:gz') as tar:
-                    tar.extractall(target)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, target)
             else:
                 with zipfile.ZipFile(archive_path) as archive:
                     archive.extractall(target)
